3e5d13b493678cc0138ae3c0b5a351748a56cffa
Vlademir Eleuterio <vla.eleut@gmail.com>
2022-02-08 10:05:24 -0300

    saving progress

---
+++ Datacamp/01 Intro to python for data science/03 python-basics-functions.py
@@ -0,0 +1,45 @@
+# A function is a piece of reausable code that solves a particular task.
+# We have buil-in functions such as type, max. But we can create our own functions as well.
+
+# Methods
+# Python objects (int, lists, strings) usually have methods. 
+# str have 'capitalize' and 'replace', for example.
+
+# Exercises - 1
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count('o'))
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.5))
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
